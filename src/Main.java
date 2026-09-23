4d17e33f8f1119da83be1f13fd1abd774c3b824a
Drishti Sahu <drishtisahu2007@gmail.com>
2026-09-23 19:11:11 +0530

    Input validation

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,53 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
-        System.out.print("Enter a number: ");
+
         Scanner in = new Scanner(System.in);
-        int num = in.nextInt();
+        int num = 0;
+        while(true) {
+            try{
+                System.out.print("Enter a number: ");
+                num = in.nextInt();
+                break;
+            } catch(InputMismatchException e){
+                System.out.println("Invalid input.");
+                in.next();
+            }
+        }
+
         int computer_num = 55;
         int count = 1;
         while(num!=computer_num) {
             if(num<computer_num) {
                 System.out.print(num + " is lower that the number! Try higher -> ");
                 count++;
-                num = in.nextInt();
+                while(true) {
+                    try{
+                        num = in.nextInt();
+                        break;
+                    } catch(InputMismatchException e){
+                        System.out.print("Invalid input.Try higher -> ");
+                        in.next();
+                    }
+                }
             } else {
                 System.out.print(num + " is higher that the number! Try lower -> ");
                 count++;
-                num = in.nextInt();
+                while(true) {
+                    try{
+                        num = in.nextInt();
+                        break;
+                    } catch(InputMismatchException e){
+                        System.out.print("Invalid input.Try lower -> ");
+                        in.next();
+                    }
+                }
             }
 
         }
         System.out.println("Hurray! you guessed it right in " + count + " attempts");
     }
+
 }
